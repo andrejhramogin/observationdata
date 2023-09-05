f6429f896c901311f56d30ddfeee4e7f5b229f07
andrejhramogin <andrej.hramogin2@gmail.com>
2023-09-05 21:11:10 +0300

    test

---
+++ src/test/java/birding/observationdata/service/observation/ObservationServiceImplTest.java
@@ -0,0 +1,30 @@
+package birding.observationdata.service.observation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class ObservationServiceImplTest {
+
+    @Test
+    void createNewObservation() {
+    }
+
+    @Test
+    void deleteObservationById() {
+    }
+
+    @Test
+    void findObservationById() {
+    }
+
+    @Test
+    void updateObservation() {
+    }
+
+    @Test
+    void getAllObservation() {
+    }
+}
